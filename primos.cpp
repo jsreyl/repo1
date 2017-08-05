0123dd23b5eb824c73a6dc9a731484cbc7201a7f
jsreyl <30728161+jsreyl@users.noreply.github.com>
2017-08-04 22:18:48 -0500

    Cambios prueba a primos.cpp combinando txt y notepad

--- primos.cpp
+++ primos.cpp
@@ -1,27 +1,17 @@
 #include <cmath>
-
 #include <iostream>
 
-
 using namespace std;
-
 int main(void){
-
-  int j = 2;
-
-  while(i%j != 0 && j<i){
-
-    j++;
-
-    if (j == i){
-
-      cout<< i <<endl;
-
-      break;
-
-    }
-
-  }
-
+  for (int i=500; i<=1400; i++){
+    int j = 2;
+    while(i%j != 0 && j<i){
+      j++;
+      if (j == i){
+	cout<< i <<endl;
+	break;
+      }
+    };
+  };
 
 }
