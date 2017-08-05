462e1e82762bcf574ad51a836cc5e35c801bf6f3
jsreyl <30728161+jsreyl@users.noreply.github.com>
2017-08-04 20:14:50 -0500

    Se crea la función que imprime un número solo si este es primo.

---
+++ primos.cpp
@@ -0,0 +1,27 @@
+#include <cmath>
+
+#include <iostream>
+
+
+using namespace std;
+
+int main(void){
+
+  int j = 2;
+
+  while(i%j != 0 && j<i){
+
+    j++;
+
+    if (j == i){
+
+      cout<< i <<endl;
+
+      break;
+
+    }
+
+  }
+
+
+}
